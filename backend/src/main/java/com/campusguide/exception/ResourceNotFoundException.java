a5243255f1a5f9db687ec27118a1d4178b86d238
Aayush Jadhav <jadhavaayush75@gmail.com>
2026-06-17 19:57:38 +0530

    feat(auth): implement authentication service

--- backend/src/main/java/com/campusguide/exception/ResourceNotFoundException.java
+++ backend/src/main/java/com/campusguide/exception/ResourceNotFoundException.java
@@ -1,2 +1,11 @@
-public class ResourceNotFoundException {
+package com.campusguide.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
--- backend/src/main/java/com/campusguide/exception/UnauthorisedException.java
+++ backend/src/main/java/com/campusguide/exception/UnauthorisedException.java
@@ -1,2 +1,11 @@
-public class UnauthorisedException {
+package com.campusguide.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class UnauthorisedException extends RuntimeException {
+    public UnauthorisedException(String message) {
+        super(message);
+    }
 }
