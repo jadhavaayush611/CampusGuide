57bf20f40a2f514834a6218b7e2381bc685e5106
Aayush Jadhav <jadhavaayush75@gmail.com>
2026-06-30 21:09:24 +0530

    feat(community): add community DTO layer

---
+++ backend/src/main/java/com/campusguide/modules/community/dto/CreateCommunityRequest.java
@@ -0,0 +1,25 @@
+package com.campusguide.modules.community.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateCommunityRequest {
+
+    @NotBlank(message = "Community name is required")
+    private String name;
+
+    @NotBlank(message = "Community description is required")
+    private String description;
+
+    private String bannerUrl;
+
+    @NotBlank(message = "Council ID is required")
+    private String councilId;
+}
---
+++ backend/src/main/java/com/campusguide/modules/community/dto/UpdateCommunityRequest.java
@@ -0,0 +1,19 @@
+package com.campusguide.modules.community.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateCommunityRequest {
+
+    private String description;
+
+    private String bannerUrl;
+
+    private Boolean isActive;
+}
