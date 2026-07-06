00cf6f1ca2d7236e7eaa89437a0f1e3ac60fab7c
Aayush Jadhav <jadhavaayush75@gmail.com>
2026-07-06 20:05:25 +0530

    feat(resource): add resource DTOs

---
+++ backend/src/main/java/com/campusguide/modules/resource/dto/ResourceSummaryResponse.java
@@ -0,0 +1,27 @@
+package com.campusguide.modules.resource.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResourceSummaryResponse {
+
+    private String id;
+
+    private String title;
+
+    private String fileType;
+
+    private Long fileSize;
+
+    private String uploaderId;
+
+    private LocalDateTime createdAt;
+}
