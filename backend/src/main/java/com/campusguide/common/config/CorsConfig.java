a9b28223347729b27d68e296cecdc33af3371097
Aayush Jadhav <jadhavaayush75@gmail.com>
2026-07-25 23:55:37 +0530

    chore: normalize line endings for config classes

--- backend/src/main/java/com/campusguide/common/config/CorsConfig.java
+++ backend/src/main/java/com/campusguide/common/config/CorsConfig.java
@@ -1,4 +1,4 @@
 package com.campusguide.common.config;
 
 public class CorsConfig {
-}
+}
--- backend/src/main/java/com/campusguide/common/config/SecurityBeansConfig.java
+++ backend/src/main/java/com/campusguide/common/config/SecurityBeansConfig.java
@@ -12,4 +12,4 @@ public class SecurityBeansConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-}
+}
